12589c540414b2ee8e684a859456fa525b9947e4
XiaoXia001 <391753186@qq.com>
2019-07-18 10:44:46 +0800

    First initialize

---
+++ src/com/zjw/base/valueTransfer.java
@@ -0,0 +1,12 @@
+package com.zjw.base;
+
+public class valueTransfer {
+
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+
+    }
+}
